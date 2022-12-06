6f5407a4abc1d62eae15085a0cfbe02030c76911
Samuel Ruiz <samuel@ruizsamuel.es>
2022-12-06 16:04:22 +0100

    Día 6
    Signed-off-by: Samuel Ruiz <samuel@ruizsamuel.es>

---
+++ src/day06/Day6A.java
@@ -0,0 +1,37 @@
+package day06;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Day6A {
+
+    public static final int N = 4;
+
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            String line = reader.readLine();
+			reader.close();
+
+            for (int i = N - 1; i < line.length(); i++) {
+                ArrayList<Character> aux = new ArrayList<Character>();
+                for (int j = 0; j < N; j++) {
+                    if(!aux.contains(line.charAt(i - j))) {
+                        aux.add(line.charAt(i - j));
+                    } else {
+                        break;
+                    }
+                }
+                if (aux.size() == N) {
+                    System.out.println("Solution: " + (i + 1));
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
